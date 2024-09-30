456ef7a5f2a5c55f269e14e6afa9543968477dc0
Rudolph Le <143877065+Rudolph-Le@users.noreply.github.com>
2024-09-30 21:15:00 +0700

    lab1
    lab1

---
+++ lab1/Ex1.java
@@ -0,0 +1,33 @@
+public class Ex1 {
+    public  static void main(String[] args)
+    {
+        if (args.length !=3){
+            System.out.println("Invalid");
+            return;
+    }
+        double val1, val2 ,result;
+        val1= Double.valueOf(args[0]);
+        val2= Double.valueOf(args[2]);
+        switch (args[1]){
+            case "+":
+                result= val1 +val2;
+                break;
+            case "-":
+                result=val1-val2;
+                break;
+            case "x":
+                result=val1*val2;
+                break;
+            case "/":
+                result=val1/val2;
+                break;
+            case "^":
+                result= Math.pow(val1, val2);
+                break;
+            default:
+                System.out.println("unsupported");
+                return;
+        }
+        System.out.println(result);
+    }
+}
